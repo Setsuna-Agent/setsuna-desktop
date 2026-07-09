d88c3cf1a63f273b1eff940288804685614a99b3
ziylike <1182900548@qq.com>
2026-07-09 10:54:28 +0800

    chore: add minWorkers configuration for CI to optimize test execution

--- vitest.release.config.ts
+++ vitest.release.config.ts
@@ -7,6 +7,7 @@ export default createVitestConfig({
   ],
   hookTimeout: 20_000,
   maxWorkers: process.env.CI ? 2 : undefined,
+  minWorkers: process.env.CI ? 1 : undefined,
   slowTestThreshold: 1_000,
   teardownTimeout: 20_000,
   testTimeout: 12_000,
